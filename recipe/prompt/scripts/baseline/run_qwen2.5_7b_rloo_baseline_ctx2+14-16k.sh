909719a39aa03ce125efe36bad64c093a1c6080a
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-04-03 08:29:08 +0000

    feat: use clean DAPO-Math

--- recipe/prompt/scripts/baseline/run_qwen2.5_7b_rloo_baseline_ctx2+14-16k.sh
+++ recipe/prompt/scripts/baseline/run_qwen2.5_7b_rloo_baseline_ctx2+14-16k.sh
@@ -56,7 +56,7 @@ RAY_DATA_HOME=${RAY_DATA_HOME:-"${HOME}/verl"}
 MODEL_HOME=${MODEL_HOME:-"${RAY_DATA_HOME}/models"}
 MODEL_PATH=${MODEL_PATH:-"${MODEL_HOME}/Qwen2.5-7B"}
 CKPTS_DIR=${CKPTS_DIR:-"${RAY_DATA_HOME}/ckpts/${project_name}/${exp_name}"}
-TRAIN_FILE=${TRAIN_FILE:-"${RAY_DATA_HOME}/data/dapo-math-unique-boxed-17k.parquet"}
+TRAIN_FILE=${TRAIN_FILE:-"${RAY_DATA_HOME}/data/dapo-math-unique-clean-17k.parquet"}
 TEST_FILE=${TEST_FILE:-"${RAY_DATA_HOME}/data/aime-2024.parquet"}
 
 ppo_mini_batch_size=${train_batch_size}
