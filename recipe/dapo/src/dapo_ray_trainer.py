da55c1eb90cd5500abf715c42e3ea8247c72edab
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-05-05 01:15:07 +0800

    feat: limiting max_tokens

--- recipe/dapo/src/dapo_ray_trainer.py
+++ recipe/dapo/src/dapo_ray_trainer.py
@@ -432,9 +432,14 @@ class RayDAPOTrainer(RayPPOTrainer):
                                 max_non_truncated_resp_len,
                                 gen_max_resp_len,
                             )
-                            max_tokens = int(
-                                max_non_truncated_resp_len
-                                * (1 + self.config.data.dynamic_max_resp_len.extending_tolerance)
+                            max_total_len = self.config.data.dynamic_max_resp_len.max_total_len
+                            max_prompt_len = self.config.data.max_prompt_length
+                            max_tokens = min(
+                                int(
+                                    max_non_truncated_resp_len
+                                    * (1 + self.config.data.dynamic_max_resp_len.extending_tolerance)
+                                ),
+                                max_total_len - max_prompt_len,
                             )
                             self.generation_state.sampling_params["max_tokens"] = max_tokens
 
