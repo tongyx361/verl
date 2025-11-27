7e308edca0c9abd0fb9db7f4a43b34f44bab8960
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-11-27 19:37:11 +0800

    Revert "feat: arbitrary val_kwargs"
    This reverts commit ed2e7342bb7a67c8ecb2e0d340ecb5641332a4a3.

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -369,9 +369,12 @@ class vLLMRollout(BaseRollout):
                 "n": 1,  # if greedy, only 1 response
             }
         elif is_validate:
+            # TODO: try **
             kwargs = {
+                "top_k": self.config.val_kwargs.top_k,
+                "top_p": self.config.val_kwargs.top_p,
+                "temperature": self.config.val_kwargs.temperature,
                 "n": 1,  # if validate, already repeat in ray_trainer
-                **self.config.val_kwargs,
             }
 
         lora_requests = None
