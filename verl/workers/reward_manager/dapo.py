2f880e433aa8132339e3c2ca43128122480d41c6
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2026-02-06 21:51:25 +0800

    fix: indexing DataProtoItem item

--- verl/workers/reward_manager/dapo.py
+++ verl/workers/reward_manager/dapo.py
@@ -72,11 +72,11 @@ class DAPORewardManager(AbstractRewardManager):
         for i in range(len(data)):
             data_item = data[i]  # DataProtoItem
 
-            prompt_ids = data_item.batch["input_ids"][:, :prompt_length]
+            prompt_ids = data_item.batch["input_ids"][:prompt_length]
             valid_prompt_length = data_item.batch["attention_mask"][:prompt_length].sum()
             valid_prompt_ids = prompt_ids[-valid_prompt_length:]
 
-            response_ids = data_item.batch["input_ids"][:, prompt_length:]
+            response_ids = data_item.batch["input_ids"][prompt_length:]
             valid_response_length = data_item.batch["attention_mask"][prompt_length:].sum()
             valid_response_ids = response_ids[:valid_response_length]
 
